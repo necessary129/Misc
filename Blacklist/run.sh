f70e1a6354fb73d712139af0719bad4919ff8e81
Shamil K Muhammed <necessary129@users.noreply.github.com>
2015-11-14 00:32:29 +0530

    Allow root only

--- Blacklist/run.sh
+++ Blacklist/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as sudo or root only!" 1>&2
+   exit 1
+fi
+
 if ls -l blacklist.txt &> /dev/null; then
     while read line; do sudo ufw delete deny from $line; done < blacklist.txt
 fi
