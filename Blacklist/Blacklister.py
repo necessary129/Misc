091e7ca030085521f0788656d2ef6451935a27b9
Shamil K Muhammed <necessary129@users.noreply.github.com>
2015-11-14 00:09:28 +0530

    Take countries from args

--- Blacklist/Blacklister.py
+++ Blacklist/Blacklister.py
@@ -1,10 +1,17 @@
+#!/usr/bin/env python3
+
 import atexit
 import subprocess
 from bs4 import BeautifulSoup as sop
 import urllib.request as reqa
 global user_agent,headers
+import argparse
 user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36'
 headers = { 'User-Agent' : user_agent }
+parser = argparse.ArgumentParser()
+parser.add_argument("countries",type=str, nargs='+',help="The countires to be blacklisted (Codes only)")
+args = parser.parse_args()
+
 def getpage(url):
     req = reqa.Request(url,None, headers)
     res = reqa.urlopen(req)
@@ -36,8 +43,10 @@ atexit.register(exi)
 def whois(asn):
         whois_str =  "whois -h whois.radb.net -- '-i origin "+asn+"' | grep -Eo \"([0-9.]+){4}/[0-9]*|([0-9A-Fa-f:]+)+/[0-9]*\""
         subprocess.call(whois_str,shell=True,stdout = blist)
+allasns = []
+for x in args.countries:
+    allasns += getcon('http://bgp.he.net/country/{}'.format(x))
 
-allasns = getcon('http://bgp.he.net/country/ID') + getcon('http://bgp.he.net/country/CN')
 for asn in allasns:
     whois(asn)
 
