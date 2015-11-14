8180680da0cd230a066f303f860dc87e7da165f7
Shamil K Muhammed <necessary129@users.noreply.github.com>
2015-11-14 13:20:06 +0530

    better script

--- Blacklist/Blacklister.py
+++ Blacklist/Blacklister.py
@@ -6,12 +6,15 @@ from bs4 import BeautifulSoup as sop
 import urllib.request as reqa
 global user_agent,headers
 import argparse
+import sys
 user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36'
 headers = { 'User-Agent' : user_agent }
 parser = argparse.ArgumentParser()
 parser.add_argument("countries",type=str, nargs='+',help="The countires to be blacklisted (Codes only)")
 args = parser.parse_args()
-
+if not args.countires:
+    print("Specify some countries!")
+    sys.exit(1)
 def getpage(url):
     req = reqa.Request(url,None, headers)
     res = reqa.urlopen(req)
@@ -44,6 +47,7 @@ def whois(asn):
         whois_str =  "whois -h whois.radb.net -- '-i origin "+asn+"' | grep -Eo \"([0-9.]+){4}/[0-9]*|([0-9A-Fa-f:]+)+/[0-9]*\""
         subprocess.call(whois_str,shell=True,stdout = blist)
 allasns = []
+print("Getting blacklists for {0}".format(" ".join(args.countries)))
 for x in args.countries:
     allasns += getcon('http://bgp.he.net/country/{}'.format(x))
 
--- Blacklist/run.sh
+++ Blacklist/run.sh
@@ -9,7 +9,8 @@ if ls -l blacklist.txt &> /dev/null; then
     while read line; do sudo ufw delete deny from $line; done < blacklist.txt
 fi
 
-python3 Blacklister.py $*;
-while read line; do sudo ufw deny from $line; done < blacklist.txt
+if python3 Blacklister.py $*; then
+    while read line; do sudo ufw deny from $line; done < blacklist.txt
+fi
 
 
